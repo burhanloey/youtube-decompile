62e75618923318f4b76917e6709bcac36d886c88
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2017-11-11 06:35:47 +0800

    Fix timestamps messed up when there is number in title and the timestamp is on right side (#7)
    * Add functions to check for timestamp location. Things still not working right when there is a line with no numbers. #7
    
    * Fix end timestamp offset when there is a line with no timestamp #7
    
    * Refactor #7
    
    * Wording #7

--- src/youtube_decompile/utils.cljs
+++ src/youtube_decompile/utils.cljs
@@ -18,19 +18,57 @@
       0 (js/parseInt timestamp)
       nil)))
 
+(defn first-timestamp
+  "Get first matched timestamp from string."
+  [line]
+  (first (re-find timestamp-regex line)))
+
+(defn last-timestamp
+  "Get last matched timestamp from string."
+  [line]
+  (first (last (re-seq timestamp-regex line))))
+
+(defn count'
+  "Count timestamps using :using function. timestamps is a list of
+  tuple(line, timestamp) like tuple(stack, needle)."
+  [timestamps & {:keys [using]}]
+  (->> timestamps
+       (filter second)
+       (filter #(apply using %))
+       count))
+
+(defn where-is-timestamp?
+  "Check whether the timestamps are on right side. Check by majority. lefties
+  and righties are lists of tuple(line, timestamp) like tuple(stack,needle).
+  Returns selected parameter."
+  [lefties & {righties :or}]
+  (let [lefties-count  (count' lefties :using string/starts-with?)
+        righties-count (count' righties :using string/ends-with?)]
+    (if (> righties-count lefties-count)
+      righties
+      lefties)))
+
+(defn lookup-timestamp
+  "Lookup timestamp from string."
+  [lines]
+  (let [lefties  (map #(vector % (first-timestamp %)) lines)
+        righties (map #(vector % (last-timestamp %)) lines)
+        selected (where-is-timestamp? lefties :or righties)]
+    (map second selected)))
+
 (defn parse-timestamps
   "Parse the inputs of timestamps.
 
   Return a list of :title, :start, and :end map."
   [timestamps]
   (let [lines            (string/split timestamps #"\n")
-        starts           (map #(first (re-find timestamp-regex %)) lines)
-        ends             (as-> (filter identity starts) %
-                           (drop 1 %)
-                           (vec %)
-                           (conj % nil)) ; nil = until the end of video
-        lines-and-starts (->> (map vector lines starts)
-                              (filter #(second %)))]
+        starts           (lookup-timestamp lines)
+        lines-and-starts (->>  (map vector lines starts) ; remove nil only after
+                               (filter second))          ; assoc w/ its origin line
+        ends             (-> (drop 1 starts)
+                             vec
+                             (conj nil)) ; nil = until the end of video
+        ]
     (->> (map conj lines-and-starts ends)
          (map #(hash-map :title (first %)
                          :start (to-seconds (second %))
