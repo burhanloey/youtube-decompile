dadedf421c2bfea0a5bbd9f0a1d5f70960f12c04
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2017-11-10 08:08:17 +0800

    Add functions to check for timestamp location. Things still not working right when there is a line with no numbers. #7

--- src/youtube_decompile/utils.cljs
+++ src/youtube_decompile/utils.cljs
@@ -18,19 +18,60 @@
       0 (js/parseInt timestamp)
       nil)))
 
+(defn first-timestamp
+  "Get first matched timestamp from string."
+  [line]
+  (first (re-find timestamp-regex line)))
+
+(defn last-timestamp
+  "Get last matched timestamp from string."
+  [line]
+  (first (last (re-seq timestamp-regex line))))
+
+(defn where-is-timestamp?
+  "Check whether the timestamps are on right side. Check by majority. lefties
+  and righties are lists of tuple(stack,needle). Returns selected parameter."
+  [& {:keys [lefties righties]}]
+  (let [lefties-count  (->> lefties
+                            (filter #(apply string/starts-with? %))
+                            count)
+        righties-count (->> righties
+                            (filter #(apply string/ends-with? %))
+                            count)]
+    (if (> righties-count lefties-count)
+      righties
+      lefties)))
+
+(defn lookup-timestamp
+  "Lookup timestamp from string."
+  [lines]
+  (let [lefties  (->> lines
+                      (map #(vector % (first-timestamp %)))
+                      (filter second))
+        righties (->> lines
+                      (map #(vector % (last-timestamp %)))
+                      (filter second))
+        selected (where-is-timestamp? :lefties lefties
+                                      :righties righties)]
+    (map second selected)))
+
 (defn parse-timestamps
   "Parse the inputs of timestamps.
 
   Return a list of :title, :start, and :end map."
   [timestamps]
   (let [lines            (string/split timestamps #"\n")
-        starts           (map #(first (re-find timestamp-regex %)) lines)
+        starts           (lookup-timestamp lines)
         ends             (as-> (filter identity starts) %
                            (drop 1 %)
                            (vec %)
                            (conj % nil)) ; nil = until the end of video
         lines-and-starts (->> (map vector lines starts)
-                              (filter #(second %)))]
+                              (filter second))]
+    (js/console.log (cljs.pprint/pprint (->> (map conj lines-and-starts ends)
+                                             (map #(hash-map :title (first %)
+                                                             :start (to-seconds (second %))
+                                                             :end   (dec (to-seconds (last %))))))))
     (->> (map conj lines-and-starts ends)
          (map #(hash-map :title (first %)
                          :start (to-seconds (second %))
