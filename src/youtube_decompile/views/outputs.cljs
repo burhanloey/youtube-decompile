a02a9a12a709fb9f642f824b22a8ee4f961c70ce
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2017-11-11 15:50:41 +0800

    Refactor #9

--- src/youtube_decompile/views/outputs.cljs
+++ src/youtube_decompile/views/outputs.cljs
@@ -4,6 +4,21 @@
             [youtube-decompile.mixins :refer [loop-video]]
             [youtube-decompile.utils :refer [parse-video-id]]))
 
+(rum/defc play-button [{:keys [display-video]}]
+  [:button
+   {:style {:margin "0 7px"}
+    :onClick #(reset! display-video true)} "play"])
+
+(rum/defc repeat-button [{:keys [repeat-video]}]
+  (let [text (if @repeat-video "repeat: yes" "repeat: no")]
+    [:button.button-outline
+     {:onClick #(swap! repeat-video not)}
+     text]))
+
+(rum/defc close-button [{:keys [display-video]}]
+  [:button.button-clear
+   {:onClick #(reset! display-video false)} "close"])
+
 (rum/defc video-frame < loop-video
   [_ {:keys [start end]}]
   (let [start' (if (and (zero? start) @state/force-at-zero?) 1 start)]
@@ -17,22 +32,14 @@
 (rum/defcs splitted-video-item < (rum/local false ::display-video)
                                < (rum/local false ::repeat-video)
   [state {:keys [title start end] :as data}]
-  (let [display-video? (::display-video state)
-        repeat-video? (::repeat-video state)
-        repeat-text (if @repeat-video? "repeat: yes" "repeat: no")]
-    [:div
-     [:h3
-      title
-      [:button
-       {:style {:margin "0 7px"}
-        :onClick #(reset! display-video? true)} "play"]
-      [:button.button-outline
-       {:onClick #(swap! repeat-video? not)}
-       repeat-text]
-      [:button.button-clear
-       {:onClick #(reset! display-video? false)} "close"]]
-     (when @display-video?
-       (video-frame {:repeat repeat-video?} data))]))
+  [:div
+   [:h3
+    title
+    (play-button {:display-video (::display-video state)})
+    (repeat-button {:repeat-video (::repeat-video state)})
+    (close-button {:display-video (::display-video state)})]
+   (when @(::display-video state)
+     (video-frame {:repeat (::repeat-video state)} data))])
 
 (rum/defc outputs < rum/reactive []
   [:div
