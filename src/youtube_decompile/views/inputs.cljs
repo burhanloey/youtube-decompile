68b5a72edc26377d0baf0fa57311a01c452e4a70
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2017-11-12 10:31:13 +0800

    Change inputs back to reactive. Also add initial values for the inputs in app-state to fix some weird behavior.

--- src/youtube_decompile/views/inputs.cljs
+++ src/youtube_decompile/views/inputs.cljs
@@ -4,13 +4,13 @@
             [youtube-decompile.app-state :as state]
             [youtube-decompile.utils :refer [parse-timestamps]]))
 
-(rum/defc youtube-url-input []
+(rum/defc youtube-url-input < rum/reactive []
   [:div
    [:label {:for "youtube-url"} "YouTube URL:"]
    [:input#youtube-url
     {:type "text"
      :onChange #(reset! state/youtube-url (-> % .-target .-value))
-     :value @state/youtube-url}]])
+     :value (rum/react state/youtube-url)}]])
 
 (rum/defc guide-text []
   [:p (str "YouTube text has some problem with line break during copy "
@@ -25,12 +25,12 @@
        {:onClick #(swap! state/force-at-zero? not)}
        (str "Force video to start from beginning: " text)]]]))
 
-(rum/defc timestamps-input []
+(rum/defc timestamps-input < rum/reactive []
   [:div
    [:label {:for "timestamps"} "Timestamps:"]
    [:textarea#timestamps
     {:onChange #(reset! state/timestamps (-> % .-target .-value))
-     :value @state/timestamps}]])
+     :value (rum/react state/timestamps)}]])
 
 (rum/defc decompile-button < rum/reactive []
   (let [current-state (rum/react state/app-state)]
